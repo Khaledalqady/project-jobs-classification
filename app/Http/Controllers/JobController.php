46b21b73557758dd6053add83e5db1765b536ff3
khaled <khaledalkady9@gmail.com>
2025-07-09 20:35:36 +0300

    Updated controllers and views, added new features, and fixed components.

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Models\Job;
 use App\Models\Tag;
+use Illuminate\Support\Arr;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\StoreJobRequest;
 use App\Http\Requests\UpdateJobRequest;
 
@@ -15,10 +18,13 @@ class JobController extends Controller
     {
         $jobs = Job::all()->groupBy('featured');
 
+        $jobs = Job::latest()->with(['employer', 'tags'])->get()->groupBy('featured');
+
+        
         return view('jobs.index', [
-            'featuredJobs' => $jobs[0],
-            'jobs' => $jobs[1],
-            'tags' => Tag::all(),
+           'featuredJobs' => $jobs[1] ?? [],
+             'jobs' => $jobs[0] ?? [],
+              'tags' => Tag::all(),
         ]);
     }
 
@@ -28,6 +34,7 @@ class JobController extends Controller
     public function create()
     {
         //
+        return view('jobs.create');
     }
 
     /**
@@ -36,30 +43,46 @@ class JobController extends Controller
     public function store(StoreJobRequest $request)
     {
         //
+        $attributes = $request->validate([
+            'title' => ['required'],
+            'salary' => ['required'],
+            'location' => ['required'],
+            'schedule' => ['required', Rule::in(['Part Time', 'Full Time'])],
+            'url' => ['required', 'active_url'],
+            'tags' => ['nullable'],
+             ]);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Job $job)
+    public function show(Job $job,$request)
     {
         //
+        $attributes['featured'] = $request->has('featured');
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Job $job)
+    public function edit(Job $job,$request,$attributes)
     {
         //
+         $job = Auth::user()->employer->jobs()->create(Arr::except($attributes, 'tags'));
+         
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateJobRequest $request, Job $job)
+    public function update(UpdateJobRequest $request, Job $job,$attributes)
     {
         //
+        if ($attributes['tags'] ?? false) {
+            foreach (explode(',', $attributes['tags']) as $tag) {
+                $job->tag($tag);
+            }
+        }
     }
 
     /**
@@ -68,6 +91,7 @@ class JobController extends Controller
     public function destroy(Job $job)
     {
         //
+        return redirect('/');
     }
 }
 
--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -1,22 +1,32 @@
 <x-panel class="flex gap-x-6">
 <div>
        <x-employer-logo />
+
+      <x-employer-logo :employer="$job->employer" />
+
+
        </div>
            
         
        <div class="flex-1 flex-col"> 
-         <a href="#" class="py-8 font-bold"iv class="self-start text-sn text-gray-400 transition-colors duration-300">Laracasts </a>
+         <a href="#" class="py-8 font-bold"iv class="self-start text-sn text-gray-400 transition-colors duration-300">{{ $job->employer->name }}</a>
        
-             <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800">Video Producer</h3>
+             <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800">{{ $job->title }}</h3>
 
-        <p class="text-sn text-gray-400 mt-auto">Full Time - From $60,000</p>
+             <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800">
+            <a href="{{ $job->url }}" target="_blank">
+                {{ $job->title }}
+            </a>
+        </h3>
+
+        <p class="text-sn text-gray-400 mt-auto">{{ $job->salary }}</p>
        
         </div>
      
             <div>
-               <x-tag>Tag</x-tag>
-               <x-tag>Tag</x-tag>
-               <x-tag>Tag</x-tag>
+              @foreach($job->tags as $tag)
+                <x-tag :$tag />
+            @endforeach
                 
             </div>
             
